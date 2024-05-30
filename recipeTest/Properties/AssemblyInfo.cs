fbd1cb7859cbcc44a91e831cc443cb7949f975f1
Elton Tati <ST10158190@vcconnect.edu.za>
2024-05-30 12:26:53 +0200

    proj done

--- recipeTest/Properties/AssemblyInfo.cs
+++ recipeTest/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("recipeTest")]
+[assembly: AssemblyTitle("RecipeTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("recipeTest")]
+[assembly: AssemblyProduct("RecipeTest")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("35cdd72f-5c36-473f-afb8-8fef5a33433a")]
+[assembly: Guid("bf54105d-ddca-4036-bc96-ee87f026a23d")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- recipeTest/UnitTest1.cs
+++ recipeTest/UnitTest1.cs
@@ -1,23 +1,21 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
-using progPart2;
+using progPart2; 
 
-namespace recipeTest
+namespace RecipeTest
 {
     [TestClass]
-    public class UnitTest1
+    public class RecipeTests
     {
         [TestMethod]
         public void TestTotalCaloriesCalculation()
         {
            
-
-            var recipe = new progPart2.Recipe("Test Recipe", 3, 3);
-            recipe.GetIngredients(3); // 3 ingredients with known calorie values
+            var recipe = new Recipe("Test Recipe", 3, 3);
+            recipe.GetIngredients(3); //  3 ingredients with known calorie values
             recipe.Ingredients[0].Calories = 100; // Ingredient 1 with 100 calories
             recipe.Ingredients[1].Calories = 200; // Ingredient 2 with 200 calories
-            recipe.Ingredients[2].Calories = 50;  // Ingredient 3 with 50 calories
+            recipe.Ingredients[2].Calories = 100;  // Ingredient 3 with 50 calories
 
             // Act
             double totalCalories = CalculateTotalCalories(recipe.Ingredients);
@@ -26,7 +24,7 @@ namespace recipeTest
             Assert.AreEqual(350, totalCalories); // Total calories should be 350 (100 + 200 + 50)
         }
 
-        private double CalculateTotalCalories(List<progPart2.Ingredient> ingredients)
+        private double CalculateTotalCalories(List<Ingredient> ingredients)
         {
             double totalCalories = 0;
             foreach (var ingredient in ingredients)
